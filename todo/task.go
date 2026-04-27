cd16289cd31a1e8fd292b8a2e3545920e53f82c7
AndreyK <kotovandrej071@gmail.com>
2026-04-27 13:55:34 +0300

    feat: implement todo business logic

---
+++ todo/task.go
@@ -0,0 +1,34 @@
+package todo
+
+import "time"
+
+type Task struct {
+	Title       string
+	Description string
+	Completed   bool
+
+	CreatedAt   time.Time
+	CompletedAT *time.Time
+}
+
+func NewRask(title string, description string) Task {
+	return Task{
+		Title:       title,
+		Description: description,
+		Completed:   false,
+
+		CreatedAt:   time.Now(),
+		CompletedAT: nil,
+	}
+}
+
+func (t *Task) Complete() {
+	completTime := time.Now()
+	t.Completed = true
+	t.CompletedAT = &completTime
+}
+
+func (t *Task) Uncomplete() {
+	t.Completed = false
+	t.CompletedAT = nil
+}
