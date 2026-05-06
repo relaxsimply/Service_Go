5399a0d64fe546f022960e0c2eebd33a5447a833
AndreyK <kotovandrej071@gmail.com>
2026-05-07 01:48:20 +0300

    07_05_2026

---
+++ postgres/simple_sql/simple_select.go
@@ -0,0 +1,58 @@
+package simple_sql
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func SelectRoes(ctx context.Context, conn *pgx.Conn) error {
+	sqlQuery := `
+	SELECT id, title, description, completed, created_at, completed_at
+	FROM tasks
+	WHERE completed = TRUE
+	`
+
+	rows, err := conn.Query(ctx, sqlQuery)
+
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	tasks := make([]TaskModel, 0)
+
+	for rows.Next() {
+		var task TaskModel
+
+		err := rows.Scan(
+			&task.ID,
+			&task.Title,
+			&task.Description,
+			&task.Completed,
+			&task.CreatedAt,
+			&task.CompletedAt,
+		)
+
+		if err != nil {
+			return err
+		}
+
+		// fmt.Println(id, title, description, completed, createdAt, completedAt)
+		// printTask(id, title, description, completed, createdAt, completedAt)
+
+	}
+
+	return nil
+}
+
+func printTask(task TaskModel) {
+	fmt.Println("------------------------------")
+	fmt.Println("id", task.ID)
+	fmt.Println("title", task.Title)
+	fmt.Println("description", task.Description)
+	fmt.Println("completed", task.Completed)
+	fmt.Println("createdAt", task.CreatedAt)
+	fmt.Println("completedAt", task.CompletedAt)
+}
